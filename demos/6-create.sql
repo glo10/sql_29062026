b3436605e1e58b86e9040b924b3b45ba44f4e102
glo10 <contact@tshimini.fr>
2026-07-01 10:53:07 +0200

    refactor: update create table

--- demos/6-create.sql
+++ demos/6-create.sql
@@ -1,10 +1,10 @@
 -- Créer une base de données
 CREATE DATABASE agence;
--- Création des tables : 
--- [la base de données],
--- nom de la table
--- colonnes associées aux types (numérique, date)
--- les contraintes (clés)
+-- Pour la création des tables, il faut : 
+-- [la base de données] : être connecté au préalable à une base de données,
+-- Définir lenom de la table
+-- Définir les colonnes et les associées aux types(numérique, date, chaîne de caractère)
+-- les contraintes (clés primaire, étrangère(s), unique, etc.)
 CREATE TABLE voiture (
 -- Liste des colonnes avec les types
 immatriculation CHAR(9),
